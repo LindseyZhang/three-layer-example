c634eddfaf566e099ac6869a7e4036706f600ea7
lindseyzhang <lindseyzlp@hotmail.com>
2019-01-17 16:45:26 +0800

    remove insert data code from db migration

--- src/main/resources/db/migration/V2019.01.16.110935__create_product_table.sql
+++ src/main/resources/db/migration/V2019.01.16.110935__create_product_table.sql
@@ -3,11 +3,3 @@ CREATE TABLE `product` (
   name varchar(50) COMMENT '产品名称',
   price float COMMENT '产品单价'
 ) DEFAULT CHARSET=utf8;
-
-INSERT INTO `product` (`name`, `price`) VALUES ('wahaha', 3);
-INSERT INTO `product` (`name`, `price`) VALUES ('shuangwaiwai', 4);
-INSERT INTO `product` (`name`, `price`) VALUES ('iPhone XR', 8000);
-INSERT INTO `product` (`name`, `price`) VALUES ('coke', 5);
-INSERT INTO `product` (`name`, `price`) VALUES ('lays', 8);
-INSERT INTO `product` (`name`, `price`) VALUES ('burger king', 35);
-INSERT INTO `product` (`name`, `price`) VALUES ('iPad', 1399);
